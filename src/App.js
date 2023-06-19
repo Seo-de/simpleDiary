a40cb3bdb6f4fe2dd75708d649f2903cdba64e6d
DC-ON\de.seo <de.seo@dc-on.co.kr>
2023-06-19 10:02:20 +0900

    한입크기 강의 다이어리 만들기

--- src/App.js
+++ src/App.js
@@ -1,24 +1,167 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {
+  // useState,
+  useRef,
+  useEffect,
+  useMemo,
+  useCallback,
+  useReducer,
+  createContext,
+} from "react";
+import "./App.css";
+import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+import Lifecycle from "./Lifecycle";
+// import OptimizeTest from "./OptimizeTest";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INIT": {
+      return action.data;
+    }
+    case "CREATE": {
+      const created_date = new Date().getTime();
+      const newItem = {
+        ...action.data,
+        created_date,
+      };
+      return [newItem, ...state];
+    }
+    case "REMOVE": {
+      return state.filter((it) => it.id !== action.targetId);
+    }
+    case "EDIT": {
+      return state.map((it) =>
+        it.id === action.targetId
+          ? {
+              ...it,
+              content: action.newContent,
+            }
+          : it
+      );
+    }
+    default:
+      return state;
+  }
+};
+
+export const DiaryStateContext = React.createContext();
+export const DiaryDispatchContext = React.createContext();
 
 function App() {
+  // const [data, setData] = useState([]);
+  // 이걸 useReducer로 관리해줄거임.
+  const [data, dispatch] = useReducer(reducer, []);
+
+  const dataId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+
+    // action의 type = 'INIT', 전달할 data = initData
+    dispatch({ type: "INIT", data: initData });
+    // setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const onCreate = useCallback(
+    (author, content, emotion) => {
+      dispatch({
+        type: "CREATE",
+        data: { author, content, emotion, id: dataId.current },
+      });
+      // const created_date = new Date().getTime();
+      // const newItem = {
+      //   author,
+      //   content,
+      //   emotion,
+      //   created_date,
+      //   id: dataId.current,
+      // };
+
+      dataId.current += 1;
+      // setData((data) => [newItem, ...data]);
+    },
+    // 이 빈배열을 그냥 넘겨서 일기를 추가하면 추가한 일기만 나오게 된다.
+    // 그래서 빈배열을 넣는 것이 아니라 함수형 update를 사용한다.
+    []
+  );
+
+  const onRemove = useCallback((targetId) => {
+    dispatch({ type: "REMOVE", targetId });
+    // const newDiaryList = data.filter((it) => it.id !== targetId);
+    // setData(newDiaryList);
+    // setData((data) => data.filter((it) => it.id !== targetId));
+  }, []);
+
+  const onEdit = useCallback((targetId, newContent) => {
+    dispatch({
+      type: "EDIT",
+      targetId,
+      newContent,
+    });
+    // setData((data) =>
+    //   data.map((it) =>
+    //     it.id === targetId ? { ...it, content: newContent } : it
+    //   )
+    // );
+  }, []);
+
+  const dispatches = {
+    onCreate,
+    onRemove,
+    onEdit,
+  };
+
+  const memoizedDispatches = useMemo(() => {
+    return { onCreate, onEdit, onRemove };
+  }, []);
+
+  const getDiaryAnalysis = useMemo(() => {
+    const goodCount = data.filter((it) => it.emotion >= 3).length;
+    const badCount = data.length - goodCount;
+    const goodRatio = (goodCount / data.length) * 100;
+    return { goodCount, badCount, goodRatio };
+  }, [data.length]);
+
+  const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    /* Provider도 컴포넌트라서 prop이 바뀌면 재생성이 된데. 그래서 onCreate, onRemove, onEdit
+     등을 보내주면 최적화가 풀리게 된다. 그럼 어떻게 하냐 또 선언을 해서 써주면 된데*/
+    <DiaryStateContext.Provider value={data}>
+      <DiaryDispatchContext.Provider value={memoizedDispatches}>
+        <div className="App">
+          {/* <OptimizeTest /> */}
+
+          {/* <Lifecycle /> */}
+          <DiaryEditor onCreate={onCreate} />
+          <div>전체 일기 : {data.length}</div>
+          <div>기분 좋은 일기 개수 : {goodCount}</div>
+          <div>기분 나쁜 일기 개수 : {badCount}</div>
+          <div>기분 좋은 일기 비율 : {goodRatio}</div>
+          {/* diaryList를 context로 값을 받아써서 필요가 없다는데;;; 맞나...? */}
+          <DiaryList
+            onEdit={onEdit}
+            onRemove={onRemove} /* diaryList={data} */
+          />
+        </div>
+      </DiaryDispatchContext.Provider>
+    </DiaryStateContext.Provider>
   );
 }
 
